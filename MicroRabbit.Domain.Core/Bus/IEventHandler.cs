397f13a861fd64cc6961604d1071bf59aaf15d95
abdelsalamabdrabo <aabdrabo@wttco.com>
2024-03-23 00:22:27 +0200

    feat: implemented interfaces and classes for Domain.Core

---
+++ MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using MicroRabbit.Domain.Core.Events;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventHandler<TEvent> : IEventHandler
+        where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+}
